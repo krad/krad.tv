0bfd56c2beab6a403ac8a20d6f09d5e85e6c0956
krad <iam@krad.io>
2018-08-23 22:58:22 -0500

    Removing unused variable.  Bumping plainview/slugline dependencies because I got bit by a production bug using string checks on constructors

--- src/views/ManageProfile/manage-profile.js
+++ src/views/ManageProfile/manage-profile.js
@@ -2,18 +2,6 @@ import React from 'react'
 import MiddleBox from '../../components/MiddleBox/middle-box'
 import ManageProfileAvatar from './manage-profile-avatar'
 import ManageProfileForm from './manage-profile-form'
-import axios from 'axios'
-
-const instance = axios.create({
-  baseURL: process.env.REACT_APP_KRAD_API_BASE_PATH,
-  timeout: 2000,
-  // withCredentials: true,
-  // credentials: 'same-origin',
-  transformResponse: (data) => {
-    return JSON.parse(data)
-  }
-})
-
 
 function ManageProfile(props) {
     const userJSON = window.localStorage.getItem('user')
