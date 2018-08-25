90663f9e77bfb2f3823395be180f5dce64086730
krad <iam@krad.io>
2018-08-25 02:03:29 -0500

    Adding axios client into a central spot for consolodation and abstraction

---
+++ src/network/client.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+const client = axios.create({
+  baseURL: process.env.REACT_APP_KRAD_API_BASE_PATH,
+  timeout: 2000,
+  withCredentials: true,
+  credentials: 'same-origin',
+  transformResponse: (data) => {
+    return JSON.parse(data)
+  }
+})
+
+export default client
