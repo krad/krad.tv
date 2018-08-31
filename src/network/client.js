2e0f452d1cff8b1c7eff1113f1a45e4e6d8112c4
krad <iam@krad.io>
2018-08-31 02:25:14 -0500

    Slow internet is the only downside to rural life

--- src/network/client.js
+++ src/network/client.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const client = axios.create({
   baseURL: process.env.REACT_APP_KRAD_API_BASE_PATH,
-  timeout: 5000,
+  timeout: 8000,
   withCredentials: true,
   credentials: 'same-origin',
   transformResponse: (data) => {
